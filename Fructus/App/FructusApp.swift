6684d3931a75b7246f74177cb40ee098a6a542a4
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-05-02 20:12:44 -0400

    added finished cardview

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Carlos Del Carpio on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
