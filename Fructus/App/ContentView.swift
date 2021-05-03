2a68cb9eb90ab39b9d3b0a70303910ac0df789ac
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-05-03 13:07:31 -0400

    settings part 1 complete

--- Fructus/App/ContentView.swift
+++ Fructus/App/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var fruits: [Fruit] = fruitsData
+    @State private var isShowingSettings: Bool = false
     
     
     var body: some View {
@@ -22,6 +23,17 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Fruits")
+            .navigationBarItems(
+                trailing:
+                    Button(action: {
+                        isShowingSettings = true
+                    }) {
+                        Image(systemName: "slider.horizontal.3")
+                    }
+                .sheet(isPresented: $isShowingSettings){
+                    SettingsView()
+                }
+            )
         }//:NAVIGATION
     }
 }
---
+++ Fructus/App/SettingsView.swift
@@ -0,0 +1,60 @@
+//
+//  SettingsView.swift
+//  Fructus
+//
+//  Created by Carlos Del Carpio on 5/3/21.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    
+    var body: some View {
+        NavigationView() {
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 20){
+                    //MARK: - Section 1
+                    GroupBox(label: SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")) {
+                        Divider().padding(.vertical, 4)
+                        
+                        HStack(alignment: .center, spacing: 10){
+                            Image("logo")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 80, height: 80)
+                                .cornerRadius(9)
+                            
+                            Text("Most fruits are naturally low in fat, sodium, and calories.  None have cholesterol. Fruits are sources of many essential nutrients, including potassium, dietary fiber, vitamins, and much more.")
+                                .font(.footnote)
+                        }
+                    }
+                    
+                    
+                    //MARK: - Section 2
+                    
+                    
+                    //MARK: - Section 3
+                    
+                    
+                }//:VSTACK
+                .navigationBarTitle(Text("Settings"), displayMode: .large)
+                .navigationBarItems(
+                    trailing:
+                        Button(action: {presentationMode.wrappedValue.dismiss()}) {
+                            Image(systemName: "xmark")
+                        }
+                )
+                .padding()
+            }//:SCROLLVIEW
+        }//:NAVIGATIONVIEW
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+            .preferredColorScheme(.dark)
+    }
+}
