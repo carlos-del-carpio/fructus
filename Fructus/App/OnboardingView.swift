9516a505fb4f60e28c554ed192a2f55adb84022d
Carlos Del Carpio <cdeclarp@uncc.edu>
2021-05-02 20:22:09 -0400

    onboarding complete

---
+++ Fructus/App/OnboardingView.swift
@@ -0,0 +1,27 @@
+//
+//  OnboardingView.swift
+//  Fructus
+//
+//  Created by Carlos Del Carpio on 5/2/21.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        TabView {
+            ForEach(0..<5) { item in
+                FruitCardView()
+            }
+            .padding(.horizontal, 20)
+        }//:TABVIEW
+        .tabViewStyle(PageTabViewStyle())
+        .padding(.vertical, 20)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
